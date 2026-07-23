bb89ad12b11317e06a8fe5ec9012c09e8a076c90
ω <1950553426@qq.com>
2026-07-24 00:53:04 +0800

    fix

--- plugins/other/maint_task.js
+++ plugins/other/maint_task.js
@@ -163,7 +163,10 @@ export class maintTask extends plugin {
       config["proxy-groups"].forEach(g => {
         if (g.proxies) g.proxies = g.proxies.filter(name => !regex.test(name))
       })
-    config.mode = "global"
+    config.mode = "rule"
+    config.rules = [
+      "MATCH,♻️自动选择"
+    ]
     try {
       await fs.writeFile(mihomo.config_path, YAML.stringify(config))
     } catch (err) {
